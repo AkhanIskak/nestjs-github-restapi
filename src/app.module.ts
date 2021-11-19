eeb48e983e29a0c414d7d5567c70bba841635d9d
iskak akhan <alex12hitman@gmail.com>
2021-11-19 19:20:54 +0600

    fixes for heroku deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './services/app.service';
 import { GithubService } from './services/github.service';
 import { GithubController } from './app.githubcontroller';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule.forRoot()],
   controllers: [AppController, GithubController],
   providers: [AppService, GithubService],
 })
